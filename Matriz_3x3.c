92b02bede18367f00e3e904cd978dcf78abab677
Andrés Tobajas Embid <116067926+ANDRESTOBAJAS@users.noreply.github.com>
2024-01-15 09:10:26 +0100

    Update Matriz_3x3.c

--- Matriz_3x3.c
+++ Matriz_3x3.c
@@ -8,10 +8,14 @@ int main(void) {
   int fil;
   int col;
   int numeros[3][3];
+  int seed;//Semilla para inicializar el generador de números al azar
+  seed=time(0);//Cargamos la semilla con el tiempo transcurrido
+  srand(seed);//Inicializamos el generador de números aleatorios
   for(fil=0;fil<3;fil++){
     for(col=0;col<3;col++){
-      printf("\nIntroduzca el elemento %d,%d: ",fil,col);
-      scanf(" %d",&numeros[fil][col]);
+      //printf("\nIntroduzca el elemento %d,%d: ",fil,col);
+      numeros[fil][col]=rand()%2;//Relleno la posicion fil, col de la matriz con un número generado al azar entre 0 y 1
+      //scanf(" %d",&numeros[fil][col]);
     }
   }
   printf("\nMATRIZ GUARDADA:");
